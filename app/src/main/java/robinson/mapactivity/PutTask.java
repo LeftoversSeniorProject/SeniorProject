cf807a10a45a3526806d25bf37d66806725a76c8
gibl3t <detsutde@gmail.com>
2017-03-30 19:26:35 -0400

    Added Async class PutTask for Rest api endpoint for Put

---
+++ app/src/main/java/robinson/mapactivity/PutTask.java
@@ -0,0 +1,82 @@
+package robinson.mapactivity;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
+/**
+ * Created by gibl3t on 3/27/17.
+ */
+
+class PutTask extends AsyncTask<Void, Void, String> {
+
+    private Exception exception;
+    private String id;
+    private User user;
+
+    private Gson GSON = new GsonBuilder().create();
+
+    public PutTask(User user){
+        this.user = user;
+        id = user.getId();
+    }
+
+    protected void onPreExecute(){
+
+    }
+
+    protected String doInBackground(Void... urls){
+
+        try{
+            URL url = new URL("http://10.35.19.212:4567/users/" + id);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try{
+                urlConnection.setRequestMethod("PUT");
+                urlConnection.setDoOutput(true);
+
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(urlConnection.getOutputStream());
+                outputStreamWriter.write(GSON.toJson(user));
+                outputStreamWriter.flush();
+
+                int responseCode = urlConnection.getResponseCode();
+                System.out.println("Sending PUT");
+                System.out.println("Response code: " + responseCode);
+                System.out.println("GSON USER: " + GSON.toJson(user));
+
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while((line = bufferedReader.readLine()) != null){
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+
+            }
+            finally{
+                urlConnection.disconnect();
+            }
+        } catch(Exception e){
+            Log.e("ERROR", e.getMessage(), e);
+            return null;
+        }
+
+    }
+
+    protected void onPostExecute(String response){
+        if(response == null){
+            response = "THERE WAS AN ERROR";
+        }
+        Log.i("INFO", response);
+        System.out.println("Put Execute User ID = " + user.getId());
+    }
+}
