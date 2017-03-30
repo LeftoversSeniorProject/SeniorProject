6b547faaf110381699bc3d8be12c770dc979c3ce
ChrisCurreri <curreric0@students.rowan.edu>
2017-03-30 19:30:30 -0400

    created a title activity class. Android studio does not know what R is

---
+++ app/src/main/java/TitleActivity.java
@@ -0,0 +1,20 @@
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
+
+
+/**
+ * Created by chriscurreri on 3/30/17.
+ */
+
+public class TitleActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.title_screen);
+
+    }
+
+}
