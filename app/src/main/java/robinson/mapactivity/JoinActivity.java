f2d08e0debe46f28c71acfd043ea44bee14e26e3
viet le <lev3@students.rowan.edu>
2017-04-03 12:28:45 -0400

    cre

--- app/src/main/java/robinson/mapactivity/JoinActivity.java
+++ app/src/main/java/robinson/mapactivity/JoinActivity.java
@@ -3,6 +3,7 @@ package robinson.mapactivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+// Create Join Button to get user ID and then send it to the host
 public class JoinActivity extends AppCompatActivity {
 
     @Override
