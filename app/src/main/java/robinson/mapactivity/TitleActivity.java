e4dce27f467607cbb588f79c655b5417dd2366e5
viet le <lev3@students.rowan.edu>
2017-04-03 15:53:20 -0400

    create title screen by adding 3 buttons

---
+++ app/src/main/java/robinson/mapactivity/TitleActivity.java
@@ -0,0 +1,13 @@
+package robinson.mapactivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TitleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+    }
+}
