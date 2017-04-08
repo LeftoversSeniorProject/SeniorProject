5f76968210924064864742b47bf48f1b0b7da9fa
ChrisCurreri <curreric0@students.rowan.edu>
2017-04-08 14:23:12 -0400

    Updated TitleActivity buttons to Start and help and hooked up the help button to the help window activity.

--- app/src/main/java/robinson/mapactivity/TitleActivity.java
+++ app/src/main/java/robinson/mapactivity/TitleActivity.java
@@ -11,40 +11,35 @@ import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 
 public class TitleActivity extends AppCompatActivity {
-    private Button btnCreate;
-    private Button btnJoin;
+    private Button btnStart;
     private Button btnHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
-        btnCreate = (Button) findViewById(R.id.create_button);
-        //btnCreate.setOnClickListener(this);
-        btnCreate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //startActivity(new Intent(getApplicationContext(), MapsActivity.class));
-                Intent intent = new Intent(TitleActivity.this, MapsActivity.class);
 
-                startActivity(intent);
-
-            }
-        });
-        btnJoin = (Button) findViewById(R.id.join_button);
-        //btnJoin.setOnClickListener(this);
-        btnJoin.setOnClickListener(new View.OnClickListener() {
+        btnStart = (Button) findViewById(R.id.start_game_button);
+        btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //startActivity(new Intent(getApplicationContext(), MapsActivity.class));
                 Intent intent = new Intent(TitleActivity.this, JoinActivity.class);
 
                 startActivity(intent);
-
-
             }
         });
 
+        btnHelp = (Button) findViewById(R.id.help_button);
+        btnHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //startActivity(new Intent(getApplicationContext(), MapsActivity.class));
+                Intent intent = new Intent(TitleActivity.this, Help.class);
+
+                startActivity(intent);
+            }
+        });
 
         Toast.makeText(this, "button set", Toast.LENGTH_LONG).show();
     }
